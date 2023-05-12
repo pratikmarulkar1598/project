0813846c9b765ec2c650ea9447b8a1b74c967d1d
pratikmarulkar1598 <pratikvbaisolutions@gmail.com>
2023-05-12 16:45:30 +0530

    Add

--- assets/data/popularData.js
+++ assets/data/popularData.js
@@ -1,107 +1,24 @@
 const popularData=[
 
-{
-    id:'1',
-    image: require('../images/pizza1.png'),
-    title:'Primavera Pizza',
-    weight:'540 gr',
-    rating:'5.0',
-    price:3.99,
-    sizeName: 'Medium',
-    sizeNumber:14,
-    crust:'Thin Crust',
-    deliveryTime:30,
-    ingredients:[
-        {
-           id:1,
-           name:'ham',
-           image: require('../images/ham.png')
-
-        },
-        {
-            id:2,
-            name:'tomato',
-            image: require('../images/tomato.png')
- 
-         },
-         {
-            id:3,
-            name:'cheese',
-            image: require('../images/cheese.png')
- 
-         },
-         {
-            id:4,
-            name:'garlic',
-            image: require('../images/garlic.png')
- 
-         },
-    ]
-
-},
-{
-    id:'2',
-    image: require('../images/pizza2.png'),
-    title:' vegetarian Pizza',
-    weight:'450 gr',
-    rating:'4.0',
-    price:5.99,
-    sizeName:'small',
-    sizeNumber:10,
-    crust:'Thick crust',
-    deliveryTime:40,
-    ingredients:[
-        
-        
-          
-          {
-             id:1,
-             name:'cheese',
-             image: require('../images/cheese.png')
-  
-          },
-          {
-             id:2,
-             name:'garlic',
-             image: require('../images/garlic.png')
-  
-          },
-
-
-        
-    ]
-   
-   },
-   {
-    id:'3',
-    image: require('../images/pizza3.png'),
-    title:' Pepperoni Pizza',
-    weight:'700 gr',
-    rating:'5.0',
-    price:9.99,
-    sizeName:'Large',
-    sizeNumber:38,
-    crust:'Thin crust',
-    deliveryTime:20,
-    ingredients:[
-
     {
-        id:1,
-        name:'tomato',
-        image: require('../images/tomato.png')
-
-     },
-     {
-        id:2,
-        name:'cheese',
-        image: require('../images/cheese.png')
-
-     },
+        id:'1',
+        image: require('../images/images1.png'),
+        price:1800000000,
+        distance:320,},
+        
+    {
+        id:'2',
+        image: require('../images/Myhome.jpg'),
+        price:1400000000,
+        distance:120,
+    },
+       {
+        id:'3',
+        image: require('../images/image2.png'),
+         price:2140000000,
+        distance:210,}
     ]
-}
-]
-
-
+    
 
 
 
